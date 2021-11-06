22273cf02e32e32bcbde5acca59d5533579ca9ca
maebeam <maebeam0@gmail.com>
2021-11-05 19:42:06 -0700

    Fix link clicks (#7)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -104,6 +104,14 @@ export class AppComponent implements OnInit {
     this.relocateForQuery();
   }
 
+  linkClicked(event: MouseEvent, query: any) {
+    event.preventDefault();
+
+    this.explorerQuery = query.toString();
+    this.resetPagination();
+    this.relocateForQuery();
+  }
+
   copy(val: string): void {
     const selBox = document.createElement('textarea');
     selBox.style.position = 'fixed';
